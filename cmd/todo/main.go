5f69d7e90d9297f9381f0825bda1627dd004e519
iMedvedevEA <iMedvedevEA@gmail.com>
2025-04-08 09:05:05 +0700

    add-config

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+
+	"skillsRockTodo/internal/config"
+)
+
 func main() {
+
+	flagConfig := flag.String("config", "./../../config/todo.env", "Configuration file")
+	flag.Parse()
+
+	if *flagConfig != "" {
+		err := godotenv.Load(*flagConfig)
+		if err != nil {
+			log.Fatal(errors.Wrap(err, "failed to load .env file"))
+		}
+
+	}
+
+	var cfg config.TodoConfig
+	if err := envconfig.Process("", &cfg); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to load configuration"))
+	}
 
 }
--- internal/infrastructure/storePostgres/storePostgres.go
+++ internal/infrastructure/storePostgres/storePostgres.go
@@ -1,1 +1,1 @@
-package storepg
+package storepostgres
